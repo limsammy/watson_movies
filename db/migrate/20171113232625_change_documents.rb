f876a18bf86d322ddb162789d8225590e4d2f299
limsammy1@gmail.com <limsammy1@gmail.com>
2017-11-14 19:21:36 -0700

    rmed deprecated migration

--- db/migrate/20171113232625_change_documents.rb
+++ db/migrate/20171113232625_change_documents.rb
@@ -1,9 +1,5 @@
 class ChangeDocuments < ActiveRecord::Migration[5.0]
   def change
     change_column :documents, :body, :text
-    remove_column :documents, :cover_letter_file_name
-    remove_column :documents, :cover_letter_content_type
-    remove_column :documents, :cover_letter_file_size
-    remove_column :documents, :cover_letter_updated_at
   end
 end
